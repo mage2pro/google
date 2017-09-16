c54d6289909d9bb7e83a382d8f81afa7101d6d4c
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-09-16 10:43:50 +0300

    no message

--- Settings.php
+++ Settings.php
@@ -1,8 +1,10 @@
 <?php
 namespace Df\Google;
+// 2015-11-27
 /** @method static Settings s() */
 final class Settings extends \Df\Config\Settings {
 	/**
+	 * 2015-11-27
 	 * @return string
 	 */
 	function clientId() {return $this->v('client_id');}
@@ -14,6 +16,7 @@ final class Settings extends \Df\Config\Settings {
 	function serverApiKey() {return $this->p('server_api_key');}
 
 	/**
+	 * 2015-11-27
 	 * @override
 	 * @see \Df\Config\Settings::prefix()
 	 * @used-by \Df\Config\Settings::v()
